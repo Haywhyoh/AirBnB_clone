cfcc0002135068cf64357fc97c3380eea3079e32
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-08-01 14:08:54 +0100

    edited the __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -5,18 +5,18 @@ from datetime import datetime
 class BaseModel():
 
     def __init__(self):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.createdat = datetime.now()
         self.updatedat = datetime.now()
-    
+
     def __str__(self):
-        return f'[{self.__class__}] ({self.id}) <{self.__dict__}>'
+        return '[{}] ({}) <{}>'.format(self.__class__.__name, self.id, self.__dict__)
 
     def save(self):
         self.updatedat = datetime.now()
 
     def to_dict(self):
-        self.__dict__["class"] = self.__class__
+        self.__dict__["class"] = self.__class__.__name__
         self.__dict__["createdat"] = self.createdat.isoformat()
         self.__dict__["updatedat"] = self.updatedat.isoformat()
         return self.__dict__
