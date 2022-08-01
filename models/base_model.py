65229550d8e34c2cb62b42fec524688a51f644bb
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-08-01 13:36:04 +0100

    created the __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,13 @@ class BaseModel():
 
     def __init__(self):
         self.id = uuid.uuid4()
-        self.createdat = datetime.now()
-        self.updatedat = datetime.now()
+        self.createdat = datetime.now().isoformat()
+        self.updatedat = datetime.now().isoformat()
     
-    
+    def __str__(self):
+        return f'[{self.__class__}] ({self.id}) <{self.__dict__}>'
+
+    def save(self):
+        
+
+    def to_dict(save):
