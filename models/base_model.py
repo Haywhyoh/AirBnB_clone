26560f76b88c5a3478f0f071dc4349f82984e98b
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-08-01 14:23:16 +0100

    changed variable name changed_at to changedat

--- models/base_model.py
+++ models/base_model.py
@@ -6,17 +6,17 @@ class BaseModel():
 
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.createdat = datetime.now()
-        self.updatedat = datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return '[{}] ({}) <{}>'.format(self.__class__.__name, self.id, self.__dict__)
 
     def save(self):
-        self.updatedat = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         self.__dict__["class"] = self.__class__.__name__
-        self.__dict__["createdat"] = self.createdat.isoformat()
-        self.__dict__["updatedat"] = self.updatedat.isoformat()
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
         return self.__dict__
