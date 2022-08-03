2b0d4ba98358663b84f85e634330deaca0f78e4d
middo1 <hamidadeshina1@gmail.com>
2022-08-03 13:37:03 +0100

    conflicts

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,6 +12,20 @@ class FileStorage(BaseModel):
     def file_path(self):
         return self.__file_path
     
-    @file_path.setter
-    def file_path(self):
-        self.__file_path = file_path
+    # @set_file_path.setter
+    # def set_file_path(self, file_path):
+    #     self.__file_path = file_path
+    
+    # @property
+    # def get_object(self, object)
+
+    def all(self):
+        return self.__objects
+    def new(self, obj):
+        if obj:
+            key = '{} {}'.format(obj.__class__.__name__, obj.id )
+            self.__objects[key] = obj
+    def reload(self):
+        pass
+    def save(self):
+        pass
