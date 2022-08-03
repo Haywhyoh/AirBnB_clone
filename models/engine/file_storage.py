48528904c74496e975cfb643d1b2cb8b7f3ff529
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-08-03 14:49:35 +0100

    removed basemode import drom filestorage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 from curses import setupterm
 from sys import settrace
-from models.base_model import BaseModel
 import json
 from os.path import exists
-class FileStorage(BaseModel):
+class FileStorage:
 
     __file_path = "file.json"
     __objects = {}
